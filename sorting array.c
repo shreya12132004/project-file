f85e1031e95dbc133ca4275f7e007a34e18a6445
Shreya <shreyadusane95@gmail.com>
2022-09-17 13:07:13 +0530

    first commit

---
+++ sorting array.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+float calc_area(int l, int w)
+{
+  return (l * w);
+}
+
+int main()
+{
+  int i;
+  int b;
+  int a[10] = {15, 30, 45, 63, 72, 80, 86, 97, 56, 25};
+  int areaList[10] = {0};
+  int length, breadth;
+  float area;
+  for (i = 0; i <= 10; i++)
+  {
+    length = a[i];
+    printf("Rectangle Name R%d ", (i + 1));
+    printf("Value of length: %d ", length);
+
+    printf("Enter the value of breadth: ");
+    scanf("%d", &breadth);
+    area = calc_area(length, breadth);
+    areaList[i] = area;
+    printf("Area is : %f ", area);
+  }
+  printf("Below is the list of elements sorted in ascending order:\n");
+  for (i = 0; i <= 10; i++)
+    printf("%d\n", a[i]);
+
+  return 0;
+}
